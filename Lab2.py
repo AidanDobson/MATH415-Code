cd14bceebf513e54535ceadb60188380780075aa
AidanDobson <97737001+AidanDobson@users.noreply.github.com>
2025-08-04 17:47:10 +1200

    Add files via upload

--- Lab2.py
+++ Lab2.py
@@ -52,7 +52,7 @@ def solve_dirichlet_bvp(initial_1,initial_2,h,function,coefs,coef_function=compu
 def function(x):
     return 0
 
-grid_points1, solution1 = solve_dirichlet_bvp((0,0),(1,1),1/100,function,(0.1,1))
+"""grid_points1, solution1 = solve_dirichlet_bvp((0,0),(1,1),1/100,function,(0.1,1))
 grid_points2, solution2 = solve_dirichlet_bvp((0,0),(1,1),1/100,function,(0.01,1))
 grid_points3, solution3 = solve_dirichlet_bvp((0,0),(1,1),1/100,function,(0.001,1))
 grid_points4, solution4 = solve_dirichlet_bvp((0,0),(1,1),1/100,function,(0.0001,1))
@@ -65,7 +65,7 @@ plt.plot(grid_points4,solution4,label = "Epsilon = 0.0001")
 plt.plot(grid_points5,solution5,label = "Epsilon = 0.00001")
 
 plt.legend()
-plt.show()
+plt.show()"""
 
 #Question 2.c
 
@@ -86,8 +86,59 @@ plt.plot(grid_points5,solution5,label = "Epsilon = 0.00001")
 plt.legend()
 plt.show()"""
 
+#Question 3.a Using the same approximation as in question 2, but with beta = 0, epsilon = 1
+"""grid_points, solution = solve_dirichlet_bvp((0,2),(1,3),1/100,function,(1,0))
+
+sin_coefficient = (3 - 2*np.cos(1))/np.sin(1)
+def exact_solution(x):
+    return 2*np.cos(x) + sin_coefficient*np.sin(x)
+
+plt.plot(grid_points,solution)
+plt.show()
+plt.plot(grid_points,exact_solution(grid_points))
+plt.show()"""
+
+#Question 3.b There are infinite solutions whenever alpha = -beta, and no solutions otherwise
+#Taking alpha = -beta = 1, we can vary the coefficient of sin(x) freely
+"""b_values = np.linspace(0,10,25)
+
+x = np.linspace(0,2*np.pi,100)
+
+for b_value in b_values:
+    plt.plot(x,np.cos(x) + b_value*np.sin(x))
+
+plt.show()"""
+
+#Question 3.c
+"""h_values = [0.9,0.5,0.1,0.01,0.001,0.0001]
+
+for h in h_values:
+    grid_points, solution = solve_dirichlet_bvp((0,1),(np.pi,-1),h,function,(1,0))
+    plt.plot(grid_points,solution,label=f"h = {h}")
+
+plt.legend()
+plt.show()"""
+#Part 2
+"""h_values = [0.9,0.5,0.1,0.01,0.001,0.0001]
+
+for h in h_values:
+    grid_points, solution = solve_dirichlet_bvp((0,1),(np.pi,1),h,function,(1,0))
+    plt.plot(grid_points,solution,label=f"h = {h}")
+
+plt.legend()
+plt.show()"""
+
+#Question 3.d
+
+h_values = [0.5,0.1,0.01,0.001]
+
+#Compute the eigenvalues of the matrix A
+"""print("\n       h              Smallest eigenvalue absolute value           matrix_norm")
+for h in h_values:
+    A = construct_tridiagonal(compute_coefficients(h,1,0),int(np.pi/h),h)
     
-
-    
-
-
+    eigenvalues = np.linalg.eig(A)[0]
+    A_inverse = np.linalg.inv(A)
+    inverse_norm = np.linalg.norm(A_inverse,2)
+    print("%13.4e   %13.4e   %13.4e" %
+              (h, min(abs(eigenvalues)),inverse_norm))"""
